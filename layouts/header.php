b551933e1be48760f1a1969cb8d07ef1ad088a7e
Vanius Ziemer <vextorz@gmail.com>
2025-05-31 12:53:13 -0300

    Add files via upload

---
+++ layouts/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <title>Consulta de Signo</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+<body>
